c372e6e62b790e1476fdaa17a14c09673223dfd9
Gauraang24 <garryappdroid@gmail.com>
2023-09-16 01:52:59 +0530

    added radio button to select between actors and shows

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,38 +1,55 @@
 import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
-import { searchForShows } from '../api/tvmaze';
+import { searchForShows,searchForPeople } from '../api/tvmaze';
 
 const Home = () => {
   const [searchString, setSearchString] = useState('');
   const [apiData, setApiData] = useState(null);
   const [apiDataError, setApiDataError] = useState(null);
+  const [searchOption,setSearchOption]=useState('shows')
 
-  console.log(apiDataError);
+  // console.log(searchOption);
 
   const onSearchInputString = ev => {
     setSearchString(ev.target.value);
   };
 
+  const onRadioChange=(ev)=>{
+    setSearchOption(ev.target.value)
+  }
+
   const onSearch = async ev => {
     ev.preventDefault();
 
-    // fetch("https://api.tvmaze.com/search/shows?q=boys")
-    // .then((response)=>{response.json()})
-    // .then((body)=>{console.log(body)})
-
-    // const resp = await fetch(`https://api.tvmaze.com/search/shows?q=${searchString}`)
-    // const body = await resp.json()
-
-    // console.log(body)
-
     try {
       setApiDataError(null);
-      const result = await searchForShows(searchString);
-      setApiData(result);
+      if(searchOption==="shows"){
+        const result = await searchForShows(searchString);
+        setApiData(result);
+      }else{
+        const result = await searchForPeople(searchString);
+        setApiData(result);
+      }
+      
     } catch (error) {
       setApiDataError(error);
     }
   };
+
+  const dataShows = (data)=>{
+    return <div key={data.show.id}>{data.show.name}</div>
+  }
+  const dataActors = (data)=>{
+    return <div key={data.person.id}>{data.person.name}</div>
+  }
+
+  // const dataElem=(data)=>{
+  //   if (searchOption==="shows"){
+  //     return <div key={data.show.id}>{data.show.name}</div>
+  //   }else{
+  //     return <div key={data.person.id}>{data.person.name}</div>
+  //   }
+  // }
 
   const renderApiData = () => {
     if (apiDataError) {
@@ -40,9 +57,12 @@ const Home = () => {
     }
 
     if (apiData) {
-      return apiData.map(data => {
-        return <div key={data.show.id}>{data.show.name}</div>;
-      });
+      // return apiData[0].show ? apiData.map(data => dataShows(data)):apiData.map(data => dataActors(data))
+      return   apiData.map(data => {
+        return apiData[0].show? dataShows(data):dataActors(data)
+        // return searchOption==="shows" ? dataShows(data):dataActors(data)
+      
+      })
     }
 
     return null;
@@ -56,6 +76,14 @@ const Home = () => {
           value={searchString}
           onChange={onSearchInputString}
         />
+         <label>
+          Shows
+          <input type="radio" name="search-option" checked={searchOption==='shows'} value="shows" onChange={onRadioChange }/>
+         </label>
+         <label>
+          Actors
+          <input type="radio" name="search-option" checked={searchOption==='actors'} value="actors" onChange={onRadioChange } />
+         </label>
         <button type="submit">Search</button>
       </form>
       <div>{renderApiData()}</div>
